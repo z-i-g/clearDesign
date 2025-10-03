d37c74093d5af39b84537e5299dbda9d8c22a4fd
Zagidullin Ayrat <zagidull007@gmail.ru>
2025-10-03 17:34:27 +0300

    ClearDesign - добавил решение 9 задания

---
+++ src/main/java/ru/zig/service/ninth/UpgradedComplexMultiThreadProcessing.java
@@ -0,0 +1,18 @@
+package ru.zig.service.ninth;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class UpgradedComplexMultiThreadProcessing {
+    private static final int SIZE = 1000000;
+    private static final int[] data = new int[SIZE];
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            data[i] = random.nextInt(100);
+        }
+
+        System.out.println(Arrays.stream(data).parallel().sum());
+    }
+}
